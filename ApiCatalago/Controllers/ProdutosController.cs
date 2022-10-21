22274e8eeb8478a25ad440c0910f2d0b9cad6927
Ágatha Almeida <74624597+agatha-dev@users.noreply.github.com>
2022-10-21 20:44:23 -0300

    Criação do metodo post

---
+++ ApiCatalago/Controllers/ProdutosController.cs
@@ -0,0 +1,54 @@
+﻿using ApiCatalago.Context;
+using ApiCatalago.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalago.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produtos>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+            if(produtos is null)
+            {
+                return NotFound("Produtos não encontrados.....");
+            }
+            return produtos;
+        }
+
+        [HttpGet("{id:int}", Name= "ObterProduto")]
+        public ActionResult<Produtos> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if(produto is null)
+            {
+                return NotFound("Produto não encontrado...");
+            }
+            return produto;
+        }
+
+        [HttpPost]
+        public  ActionResult<Produtos> Post(Produtos produto)
+        {
+            if (produto is null)
+                return BadRequest();
+
+            _context.Produtos.Add(produto);
+            _context.SaveChangesAsync();
+
+            return CreatedAtAction("ObterProduto", new { id = produto.ProdutoId }, produto);
+     
+        }
+    }
+}
